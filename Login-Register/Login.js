7e0e5540b56548ece08d0d1f65005e9e266fa49d
badlzi <155917158+badlzi@users.noreply.github.com>
2024-08-01 03:12:20 +0700

    comfit

--- Login-Register/Login.js
+++ Login-Register/Login.js
@@ -1,13 +1,38 @@
+var check = false;
+function thaydou(kt, event){
+  const storedDataJSON = localStorage.getItem("userdata");
+  userData = JSON.parse(storedDataJSON) || [];
+  // const emails = document.getElementById('email').value;
+  // const passwords = document.getElementById('password').value;
+  // for (let is = 0; is <= userData?.length - 1; is++) {
+  //   console.log(userData[is].password);
+  //   console.log(userData[is].email);
+  //   if (emails == userData[is].email && passwords == userData[is].password) {
+  //     var use = userData[is].name
+  //     kt = true;
+  //     break;
+  //   }
+  // }
+  if(!kt){
+    document.getElementById('login-register').style.display = 'none';
+    document.getElementById('login-out').style.display = 'block';
+    document.getElementById('use').textContent = userData[0].name;
+  }else{
+    document.getElementById('login-register').style.display = 'block';
+    document.getElementById('login-out').style.display = 'none';
+  }
+}
+thaydou(check);
 function validateForm(event) {
   event.preventDefault(); // Ngăn form submit tự động
   // Lấy giá trị từ các trường nhập liệu
-  let notifications = document.querySelector('.notifications');
+  let notifications = document.querySelector(".notifications");
   const emails = document.forms["registrationForm"]["email"].value;
   const passwords = document.forms["registrationForm"]["password"].value;
   var audio = document.getElementById("myAudio");
   var emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
   function createToast(type, icon, title, text) {
-    let newToast = document.createElement('div');
+    let newToast = document.createElement("div");
     newToast.innerHTML = `
           <div class="toast ${type}">
               <i class="${icon}"></i>
@@ -18,22 +43,20 @@ function validateForm(event) {
               <i class="fa-solid fa-xmark" onclick="(this.parentElement).remove()"></i>
           </div>`;
     notifications.appendChild(newToast);
-    newToast.timeOut = setTimeout(
-      () => newToast.remove(), 5000
-    )
+    newToast.timeOut = setTimeout(() => newToast.remove(), 5000);
   }
   function Thongbao() {
-    let type = 'success';
-    let icon = 'fa-solid fa-circle-check';
-    let title = 'Thông báo';
-    let text = 'Đăng nhập thành công';
+    let type = "success";
+    let icon = "fa-solid fa-circle-check";
+    let title = "Thông báo";
+    let text = "Đăng nhập thành công";
     createToast(type, icon, title, text);
   }
   function BaoLoi() {
-    let type = 'error';
-    let icon = 'fa-solid fa-circle-exclamation';
-    let title = 'Lỗi';
-    let text = 'Email hoặc mật khẩu không đúng.';
+    let type = "error";
+    let icon = "fa-solid fa-circle-exclamation";
+    let title = "Lỗi";
+    let text = "Email hoặc mật khẩu không đúng.";
     createToast(type, icon, title, text);
   }
   function Loidetrong() {
@@ -43,49 +66,55 @@ function validateForm(event) {
     let text = "Vui lòng nhập đầy đủ thông tin";
     createToast(type, icon, title, text);
   }
-  function loiEmail(){
+  function loiEmail() {
     let type = "error";
     let icon = "fa-solid fa-circle-exclamation";
     let title = "Lỗi";
     let text = "Email nhập không đúng định dạng";
     createToast(type, icon, title, text);
   }
-  const storedDataJSON = localStorage.getItem("userdata")
-  userData = JSON.parse(storedDataJSON) || []
-  let check = false
-  if (emails === '' || passwords === '') {
+  const storedDataJSON = localStorage.getItem("userdata");
+  userData = JSON.parse(storedDataJSON) || [];
+  if (emails === "" || passwords === "") {
     Loidetrong();
     return;
   }
-  if(!emailPattern.test(emails)){
-    loiEmail();  
+  if (!emailPattern.test(emails)) {
+    loiEmail();
     return;
   }
+  let check = true;
   for (let is = 0; is <= userData?.length - 1; is++) {
     console.log(userData[is].password);
     console.log(userData[is].email);
     if (emails == userData[is].email && passwords == userData[is].password) {
-      check = true
+      check = true;
       break;
     }
-
   }
 
   if (check) {
     Thongbao();
     audio.play();
-    return;
-
+    kt = check;
+    setTimeout(window.location.href = "../Home/index.html", 1000)
+    return check;
   } else {
     BaoLoi();
   }
-
 }
-document.getElementById('toggle-password').addEventListener('click', function () {
-  var passwordInput = document.getElementById('password');
-  var type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
-  passwordInput.setAttribute('type', type);
-
-  // Thay đổi biểu tượng con mắt khi nhấn
-  this.innerHTML = type === 'password' ? '<i class="fa-solid fa-eye-slash"></i>' : '<i class="fa-solid fa-eye"></i>'; // Sử dụng mã Unicode cho các biểu tượng khác nhau
+document.querySelectorAll(".toggle-password").forEach((toggle) => {
+  toggle.addEventListener("click", function () {
+    let input = this.previousElementSibling;
+    let icon = this.querySelector("i");
+    if (input.type === "password") {
+      input.type = "text";
+      icon.classList.remove("fa-eye-slash");
+      icon.classList.add("fa-eye");
+    } else {
+      input.type = "password";
+      icon.classList.remove("fa-eye");
+      icon.classList.add("fa-eye-slash");
+    }
+  });
 });
